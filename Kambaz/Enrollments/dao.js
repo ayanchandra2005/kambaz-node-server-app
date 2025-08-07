5c8cab1f14e2804b4c356423a2dfc59236ab6331
ayanchandra2005 <ayanchandra2005@gmail.com>
2025-08-07 19:26:47 -0400

    Another 1

--- Kambaz/Enrollments/dao.js
+++ Kambaz/Enrollments/dao.js
@@ -1,24 +1,24 @@
-import Database from "../Database/index.js";
-import { v4 as uuidv4 } from "uuid";
+// import Database from "../Database/index.js";
+// import { v4 as uuidv4 } from "uuid";
 
-let { enrollments } = Database;
+// let { enrollments } = Database;
 
-export function enrollUserInCourse(userId, courseId) {
-  const exists = enrollments.some(
-    (e) => e.user === userId && e.course === courseId
-  );
-  if (!exists) {
-    enrollments.push({ _id: uuidv4(), user: userId, course: courseId });
-  }
-}
+// export function enrollUserInCourse(userId, courseId) {
+//   const exists = enrollments.some(
+//     (e) => e.user === userId && e.course === courseId
+//   );
+//   if (!exists) {
+//     enrollments.push({ _id: uuidv4(), user: userId, course: courseId });
+//   }
+// }
 
-export function unenrollUserFromCourse(userId, courseId) {
-  enrollments = enrollments.filter(
-    (e) => !(e.user === userId && e.course === courseId)
-  );
-  Database.enrollments = enrollments; 
-}
+// export function unenrollUserFromCourse(userId, courseId) {
+//   enrollments = enrollments.filter(
+//     (e) => !(e.user === userId && e.course === courseId)
+//   );
+//   Database.enrollments = enrollments; 
+// }
 
-export function findEnrollmentsByUserId(userId) {
-  return enrollments.filter((e) => e.user === userId);
-}
+// export function findEnrollmentsByUserId(userId) {
+//   return enrollments.filter((e) => e.user === userId);
+// }
--- Kambaz/Users/routes.js
+++ Kambaz/Users/routes.js
@@ -1,6 +1,6 @@
 import * as dao from "./dao.js";
 import * as courseDao from "../Courses/dao.js";
-import * as enrollmentsDao from "../Enrollments/Dao.js";
+import * as enrollmentsDao from "../Enrollments/dao1.js";
 
 export default function UserRoutes(app) {
   const createUser = (req, res) => {};
